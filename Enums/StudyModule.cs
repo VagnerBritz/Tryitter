0088a781fbfaaa27a04b96e95afceb5c3760901c
Vagner Britz <vagnerbritz@gmail.com>
2022-12-13 12:42:55 -0300

    feat: Create the model classes: User and Post

---
+++ Enums/StudyModule.cs
@@ -0,0 +1,10 @@
+﻿namespace Tryitter.Enums
+{
+    enum StudyModule
+    {
+        Fundamentos,
+        FrontEnd,
+        BackEnd,
+        CienciaDaComputacao
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tryitter.Models
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string? Message { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public User? User { get; set; }
+        public int UserId { get; set; }
+    }
+}
